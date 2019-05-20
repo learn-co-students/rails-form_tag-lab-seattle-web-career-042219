b72500235145b097bacf376566dae29a0ade5fe5
Greg Morgan <gregory.morgan@flatironschool.com>
2019-05-20 09:49:07 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 class StudentsController < ApplicationController
-  
   def index
     @students = Student.all
   end
@@ -8,12 +9,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def new
-  end
+  def new; end
 
   def create
-    session[:form_params] = params.inspect
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to new_student_path
   end
-
 end
